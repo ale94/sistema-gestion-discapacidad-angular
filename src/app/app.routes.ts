93d4e5d173df8a544213b95e999f3104cb265d58
ale94 <alejandrorua111@gmail.com>
2025-12-04 09:38:29 -0300

    feat: define application routes for login and fallback redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/pages/login-page/login-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
