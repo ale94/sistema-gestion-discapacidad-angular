f2eac0f80b9b19729ec9244851a1c5a2bb3e2075
ale94 <alejandrorua111@gmail.com>
2025-12-04 09:54:52 -0300

    feat: update the login form

--- src/app/features/login/pages/login-page/login-page.ts
+++ src/app/features/login/pages/login-page/login-page.ts
@@ -1,17 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'login-page',
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './login-page.html',
 })
 export default class LoginPage {
+
   private authService = inject(Auth);
-  // FIX: Explicitly type `router` to resolve `Property 'navigate' does not exist on type 'unknown'`.
   private router: Router = inject(Router);
 
   username = signal('');
