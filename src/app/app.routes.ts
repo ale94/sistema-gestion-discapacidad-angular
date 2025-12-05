5294968f51366caaf4af3fcb3c4e1d0358741d06
ale94 <alejandrorua111@gmail.com>
2025-12-04 23:12:59 -0300

    feat: update routes and correct the login component import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,46 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './shared/services/auth.service';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./features/login/pages/login-page/login-page'),
+    loadComponent: () => import('./features/login/login-page'),
+  },
+  {
+    path: '',
+    canActivate: [authGuard],
+    loadComponent: () => import('./shared/layouts/main-layout'),
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard-page')
+      },
+      {
+        path: 'personas',
+        loadComponent: () => import('./features/person/list/person-list')
+      },
+      {
+        path: 'personas/:id',
+        loadComponent: () => import('./features/person/profile/person-profile')
+      },
+      {
+        path: 'graficos',
+        loadComponent: () => import('./features/chart/chart-page')
+      },
+      {
+        path: 'transporte',
+        loadComponent: () => import('./features/transport/transport-tracking')
+      },
+      {
+        path: 'eventos',
+        loadComponent: () => import('./features/event/list/list')
+      },
+    ]
   },
   {
     path: '**',
