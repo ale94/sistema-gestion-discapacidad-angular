f9fd1a6e819ca05b6e3317c5d61fa3407a6bec58
ale94 <alejandrorua111@gmail.com>
2025-12-05 09:11:51 -0300

    feat: display user profile image and name in header component

--- src/app/shared/components/head/head.ts
+++ src/app/shared/components/head/head.ts
@@ -1,11 +1,14 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-head',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './head.html',
 })
 export class Head {
   toggleMobileSidebar = output<void>();
+  authService = inject(AuthService);
 }
