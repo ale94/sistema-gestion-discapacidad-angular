4119dbe6761ca28031f7981867d5f3611797d954
ale94 <alejandrorua111@gmail.com>
2025-12-10 22:08:20 -0300

    feat: configure application locale to Spanish and update HTML language attribute

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-AR'
+
+registerLocaleData(localeEs, "es")
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideCharts(withDefaultRegisterables())
+    provideCharts(withDefaultRegisterables()),
+    {
+      provide: LOCALE_ID,
+      useValue: "es"
+    }
   ]
 };
