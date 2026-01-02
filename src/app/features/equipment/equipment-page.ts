1a22d9af98566394c32ca690355b691c9f03e27d
ale94 <alejandrorua111@gmail.com>
2026-01-01 23:34:47 -0300

    feat: Implement initial routing and sidebar navigation, adding placeholder components for equipment and person indicators.

---
+++ src/app/features/equipment/equipment-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'equipment-page',
+  imports: [],
+  templateUrl: './equipment-page.html',
+})
+export default class EquipmentPage {}
---
+++ src/app/features/person/indicator/person-indicator.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'person-indicator',
+  imports: [],
+  templateUrl: './person-indicator.html',
+})
+export default class PersonIndicator {}
