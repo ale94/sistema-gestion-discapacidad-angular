d8068c42807576052ab399c53c8ce1a8e51e8eeb
ale94 <alejandrorua111@gmail.com>
2026-01-02 23:07:02 -0300

    feat: implement new feature for managing people with indicators with dedicated UI, component, service, and interface.

--- src/app/features/person/indicator/person-indicator.ts
+++ src/app/features/person/indicator/person-indicator.ts
@@ -1,8 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { PeopleIndicatorsService } from '../../../shared/services/people.indicators.service';
+import { PeopleIndicator } from '../../../shared/interfaces/people.indicator.interface';
+import { DecimalPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'person-indicator',
-  imports: [],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './person-indicator.html',
 })
-export default class PersonIndicator {}
+export default class PersonIndicator {
+  private peopleIndicators = inject(PeopleIndicatorsService);
+  people = this.peopleIndicators.getPeopleIndicators();
+  isModalOpen = signal(false);
+  editingPerson = signal<PeopleIndicator | null>(null);
+  personToDelete = signal<PeopleIndicator | null>(null);
+
+  searchTerm = signal('');
+
+  filteredPeopleIndicators = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+    const people = this.people();
+
+    return people.filter((person) => {
+      if (!term) return true;
+
+      return person.apellidoNombre.toLowerCase().includes(term) || person.dni.includes(term);
+    });
+  });
+
+  onSearchChange(term: string) {
+    this.searchTerm.set(term);
+  }
+
+  openAddModal() {
+    this.editingPerson.set(null);
+    this.isModalOpen.set(true);
+  }
+
+  openEditModal(person: PeopleIndicator) {
+    this.editingPerson.set(person);
+    this.isModalOpen.set(true);
+  }
+
+  requestDelete(person: PeopleIndicator): void {
+    this.personToDelete.set(person);
+  }
+
+  confirmDeleteAction(): void {
+    if (this.personToDelete()) {
+      this.peopleIndicators.deletePerson(this.personToDelete()!.id);
+      this.cancelDelete();
+    }
+  }
+
+  cancelDelete(): void {
+    this.personToDelete.set(null);
+  }
+
+  handleSave(personData: Omit<PeopleIndicator, 'id'> | PeopleIndicator) {
+    if ('id' in personData) {
+      this.peopleIndicators.updatePerson(personData);
+    } else {
+      this.peopleIndicators.addPerson(personData);
+    }
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
+    this.editingPerson.set(null);
+  }
+}
