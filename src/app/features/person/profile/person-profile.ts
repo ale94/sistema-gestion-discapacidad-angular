6c4e42eb35727c46859b34da7d37e5890a8ca7e5
ale94 <alejandrorua111@gmail.com>
2026-03-10 23:58:47 -0300

    refactor: enhance person profile template and component with title case formatting and family member details

--- src/app/features/person/profile/person-profile.ts
+++ src/app/features/person/profile/person-profile.ts
@@ -1,12 +1,13 @@
 import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PersonService } from '../../../shared/services/person.service';
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Title } from 'chart.js';
 
 @Component({
   selector: 'person-profile',
   standalone: true,
-  imports: [RouterLink, DecimalPipe],
+  imports: [RouterLink, DecimalPipe, TitleCasePipe, UpperCasePipe],
   templateUrl: './person-profile.html',
 })
 export default class PersonProfile {
@@ -16,9 +17,9 @@ export default class PersonProfile {
   person = computed(() => {
     const personId = this.route.snapshot.paramMap.get('id');
     if (!personId) return undefined;
-    // return this.personService
-    //   .getPeople()()
-    //   .find((p) => p.id === +personId);
+    return this.personService
+      .persons()
+      .find((p) => p.id === +personId);
   });
 
   getAge(birthDate: string | undefined): number | null {
